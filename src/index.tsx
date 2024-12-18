97683fa338e608ad606926b42b6a7621fbd3c155
Dybus8 <135601513+Dybus8@users.noreply.github.com>
2024-12-18 13:10:14 +0100

    Routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,11 @@
 import { createRoot } from 'react-dom/client'
-import { App } from './App'
+import { App, Book } from './App'
 import { BrowserRouter, Route, Routes } from 'react-router'
+import { HomePage } from './pages/HomePage'
+import { About } from './pages/About'
+import { Contact } from './pages/Contact'
+import { BookDetail } from './pages/BookDetail'
+import { ApiData } from './dataLoaders'
 
 const domNode = document.getElementById('root')
 if (domNode != null) {
@@ -9,7 +14,26 @@ if (domNode != null) {
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<App />} />
-            
+                <Route path='/home' element={<HomePage
+                        selectedFilter={'title'} search={''} library={[]} onRefresh={function (): void {
+                            throw new Error('Function not implemented.')
+                        } } onSearch={function (): void {
+                            throw new Error('Function not implemented.')
+                        } } />} />
+                <Route path='/about' element={<About />} />
+                <Route path='/contact' element={<Contact />} />
+                <Route path='/book/:id' element={<BookDetail
+                        book={{
+                            kind: 'rejected',
+                            errorMessage: undefined
+                        }} onGoBack={function (): void {
+                            throw new Error('Function not implemented.')
+                        } } onLoaded={function (data: ApiData<Book>): void {
+                            throw new Error('Function not implemented.')
+                        } } onRetry={function (): void {
+                            throw new Error('Function not implemented.')
+                        } } />} />
+                <Route path='*' element={<div>404 Not Found</div>} />
             </Routes>
         </BrowserRouter>
     )
